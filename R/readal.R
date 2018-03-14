2781e0956cb9b2ba90e911869ca6069926b24c8d
helloicyvodka <22588620+helloicyvodka@users.noreply.github.com>
2018-03-14 12:36:10 +0800

    readal.alml2

--- R/readal.R
+++ R/readal.R
@@ -91,7 +91,6 @@ readal.alm<-function(fileS,fileT){
 #' @import ggtree
 
 
-
 readal.alml<-function(file){
 
   if(!exists("alm_label")){
@@ -109,7 +108,7 @@ readal.alml<-function(file){
 
 
 
-  for(i in 1:nl){
+  for(i in 1:nl-1){
 
     the_line<-the_text[[i]]
 
@@ -179,6 +178,127 @@ readal.alml<-function(file){
   return(final_result)
 
   }
+
+
+
+#----------------------------------------------------------------------------------------------
+
+#' Read local alignment results from HSA calculation and transform the result from txt to list in R
+#'
+#' @title readal.alml2
+#' @author Meng Yuan
+#' @param file file address of alml result (with random tree alignment pvalue)
+#' @return a list containing the result (phylos of treeS and treeT, the matching dataframe) and the result analysis (pValue and etc.)
+#' @export
+#' @import rlist
+#' @import plyr
+#' @import dplyr
+#' @import ggtree
+
+#file<-"../yangjr_test/01.treeS.alml"
+
+readal.alml2<-function(file){
+
+  if(!exists("alm_label")){
+    stop("Please run readal.alm firstly and create the alm_label varaiable! ATTENTION: readal.alm will creates alm_label variable automately.")
+  }
+
+
+  the_result<-list()
+
+  the_prefix<-c("Score","RootS","RootT","PruneS","PruneT","MatchS","MatchT","}","PValue","Min")
+
+  the_text<-readLines(file)
+
+  nl<-length(the_text)
+
+
+
+  for(i in 1:(nl-1)){
+
+    the_line<-the_text[[i]]
+
+
+    if(regexpr("^[0-9]",the_line)==T){
+
+      num<-as.character(regmatches(the_line,regexpr("^([0-9]+)",the_line)))
+
+      if(startsWith(the_text[[(i+1)]],"Score")==T){
+
+        if(length(num)>0){
+          the_result[[num]]<-list()
+          the_result[[num]]<-list("score_order"=as.numeric(num))
+        }
+      }
+
+    }else{
+
+        for(i2 in 1:(length(the_prefix)-3)){
+
+          if(startsWith(the_line,the_prefix[i2])){
+
+            the_result[[num]][[as.character(the_prefix[i2])]]<-unlist(strsplit(the_line,split = ":"))[2]
+
+            }
+        }
+
+
+        if(startsWith(the_line,"PValue")){
+
+          the_result[[num]][["PValue"]]<-list()
+
+          the_result[[num]][["PValue"]][["all"]]<-unlist(strsplit(the_line,split = ":"))[2]
+
+        }
+
+        if(startsWith(the_line,"Min")){
+
+          the_result[[num]][["PValue"]][["Min"]]<-unlist(strsplit(unlist(strsplit(the_line,split = " "))[1],split = ":"))[2]
+
+          the_result[[num]][["PValue"]][["Max"]]<-unlist(strsplit(unlist(strsplit(the_line,split = " "))[2],split = ":"))[2]
+
+          the_result[[num]][["PValue"]][["AVG"]]<-unlist(strsplit(unlist(strsplit(the_line,split = " "))[3],split = ":"))[2]
+
+          the_result[[num]][["PValue"]][["pvalue"]]<-unlist(strsplit(unlist(strsplit(the_line,split = " "))[4],split = ":"))[2]
+
+        }
+      }
+
+  }
+
+
+  for(i in 1:length(the_result)){
+    class(the_result[[i]])<-c("alml",class(the_result[[i]]))
+  }
+
+  class(the_result)<-c("alml_list",class(the_result))
+
+  return(the_result)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
